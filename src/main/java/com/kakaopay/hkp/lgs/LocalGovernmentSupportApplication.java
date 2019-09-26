2676a33c5249334fc7d800f4ddd27b449454e676
KwangPil Hong <hkpking@tmon.co.kr>
2019-09-26 11:47:44 +0900

    init

---
+++ src/main/java/com/kakaopay/hkp/lgs/LocalGovernmentSupportApplication.java
@@ -0,0 +1,13 @@
+package com.kakaopay.hkp.lgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalGovernmentSupportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalGovernmentSupportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kakaopay/hkp/lgs/LocalGovernmentSupportApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kakaopay.hkp.lgs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LocalGovernmentSupportApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
