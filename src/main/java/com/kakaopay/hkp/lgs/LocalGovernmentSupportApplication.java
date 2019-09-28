317dab862c62424f6a6e6cf6f3b12e965f7e29be
Unknown <hkpking@tmon.co.kr>
2019-09-28 15:41:29 +0900

    Add application properties

--- src/main/java/com/kakaopay/hkp/lgs/LocalGovernmentSupportApplication.java
+++ src/main/java/com/kakaopay/hkp/lgs/LocalGovernmentSupportApplication.java
@@ -1,9 +1,12 @@
 package com.kakaopay.hkp.lgs;
 
+import com.kakaopay.hkp.lgs.config.ApplicationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({ApplicationProperties.class})
 public class LocalGovernmentSupportApplication {
 
     public static void main(String[] args) {
