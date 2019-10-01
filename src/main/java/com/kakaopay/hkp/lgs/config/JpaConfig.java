da5559ac99835baa638d6a0cd3abb22fc2f485f9
Unknown <hkpking@tmon.co.kr>
2019-10-01 15:23:52 +0900

    세팅 추가 - JPA @EnableSpringDataWebSupport
    도메인 클래스 컨버터, 페이징, 정렬 사용가능

--- src/main/java/com/kakaopay/hkp/lgs/config/JpaConfig.java
+++ src/main/java/com/kakaopay/hkp/lgs/config/JpaConfig.java
@@ -2,8 +2,10 @@ package com.kakaopay.hkp.lgs.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @EnableJpaAuditing
+@EnableSpringDataWebSupport
 @Configuration
 public class JpaConfig {
 }
