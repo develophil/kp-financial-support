653c5eb2c99a466701cabb4b13f288f9b16290dc
Unknown <hkpking@tmon.co.kr>
2019-10-13 15:30:25 +0900

    설정 변경 - swagger global setting : authorization

--- src/main/java/com/kakaopay/hkp/lgs/config/SwaggerConfig.java
+++ src/main/java/com/kakaopay/hkp/lgs/config/SwaggerConfig.java
@@ -8,11 +8,14 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Configuration
@@ -22,7 +25,17 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
 
+        List<Parameter> global = Collections.singletonList(new ParameterBuilder()
+                .name("Authorization")
+                .description("JWT Token")
+                .parameterType("header")
+                .required(false)
+                .defaultValue("bearer ")
+                .modelRef(new ModelRef("string"))
+                .build());
+
         return new Docket(DocumentationType.SWAGGER_2)
+                .globalOperationParameters(global)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.kakaopay.hkp.lgs"))
                 .paths(PathSelectors.any())
